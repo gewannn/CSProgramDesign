6843f844fd94bf214b16a553d3afbbc78d88c495
gewannn <geguan96@gami.com>
2025-10-04 20:25:44 +0800

    相似度小于30%的物品不进行输出

--- itemdetect.py
+++ itemdetect.py
@@ -27,10 +27,13 @@ while True:
     idxs = np.argsort(preds[0])[::-1][:3]
 
     for (i, idx) in enumerate(idxs):
+        score = float(preds[0][idx]) #可信度
         if i == 0 and preds[0][idx] >= 0.3: #排名第一，且可信度不小于30%
             text = "{}, {:.2%}".format(classes[idx], preds[0][idx])
             cv2.putText(image, text, (5, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2) #叠加文字
-        print("[{}] {}, {:.2%}".format(i+1, classes[idx], preds[0][idx]))
+        
+        if score >= 0.3: #可信度不小于30%进行输出
+            print("[{}] {}, {:.2%}".format(i+1, classes[idx], preds[0][idx]))
     
     cv2.imshow("Image", image)
 
